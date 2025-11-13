987aac7dd3063731450d936b9b612e8ac53c07fe
Amulya768 <amulya.c@dayforce.com>
2025-11-13 10:07:14 +0000

    css_changes

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -39,7 +39,7 @@ class Command(BaseCommand):
         teams_col.insert_one({
             'id': 2,
             'name': 'dc',
-            'description': 'Team DC1',
+            'description': 'Team DC',
             'created_at': now_iso
         })
         marvel_id = 1
